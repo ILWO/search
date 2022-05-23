a3ca685197ba1af972f796bb646911346d6a4c5d
ILWO <106041793+ILWO@users.noreply.github.com>
2022-05-23 00:36:35 -0700

    Internet Library of Web Organization

---
+++ script/main.js
@@ -0,0 +1,44 @@
+const searchInput = document.querySelector("#search-input");
+
+searchInput.addEventListener("keydown", function(event) {
+    if(event.code === "Enter") {
+        search();
+    }
+});
+
+function search() {
+    const input = searchInput.value;
+
+    window.location.href = "https://www.google.com/search?q=" + input + "&rlz=1C5CHFA_enNZ948NZ948&oq=" + input + "&aqs=chrome.0.69i59l2j46i175i199i433j46i199i291i433j46j0i433j0j69i60.875j0j9&sourceid=chrome&ie=UTF-8"
+}
+
+let query = document.querySelector('.search-input');
+let searchBtn = document.querySelector('.google-search-btn');
+
+searchBtn.onclick = function(){
+    let url = 'https://www.google.com/search?q='+query.value;
+    window.open(url,'_self');
+}
+
+setInterval(()=>{
+    const time = document.querySelector(".display #time");
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let day_night = "AM";
+    if(hours > 12){
+      day_night = "PM";
+      hours = hours - 12;
+    }
+    if(seconds < 10){
+      seconds = "0" + seconds;
+    }
+    if(minutes < 10){
+      minutes = "0" + minutes;
+    }
+    if(hours < 10){
+      hours = "0" + hours;
+    }
+    time.textContent = hours + ":" + minutes + ":" + seconds + " "+ day_night;
+  });
